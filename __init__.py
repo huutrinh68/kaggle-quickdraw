87d87356099df66c4940cc13a893328fc75d0e99
Nguyen Huu Trinh <trinhsp89@gmail.com>
2018-10-24 07:00:47 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,17 @@
+"""
+This project include source code for quick-draw kaggle compettion.
+
+    :platform: ubuntu 16.04
+    
+"""
+
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 
-__version__ = '0.1'
+__author__ = """Nguyen Huu Trinh"""
+__email__ = 'trinhnh1@fsoft.com.vn'
+__version__ = '0.0.1'
+
+
