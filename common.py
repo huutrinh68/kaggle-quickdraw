20057f94e7240df72b80343fe4b99772828993c5
Nguyen Huu Trinh <trinhsp89@gmail.com>
2018-10-24 05:28:52 +0000

    add common library

---
+++ common.py
@@ -0,0 +1,32 @@
+"""
+    import common library
+"""
+import os
+import ast
+import datetime as dt
+
+# matplot and graph
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = [16, 10]
+plt.rcParams['font.size'] = 14
+import seaborn as sns
+
+# as usual
+import seaborn as sns
+import cv2
+import pandas as pd
+import numpy as np
+
+# tensorflow and keras
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation
+from tensorflow.keras.metrics import categorical_accuracy, top_k_categorical_accuracy, categorical_crossentropy
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.applications.mobilenet import preprocess_input
+
+start = dt.datetime.now()
