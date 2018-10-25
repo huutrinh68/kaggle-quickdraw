8b27d4e6a41b46f15e66b2523cbaa960cb5306f6
Nguyen Huu Trinh <trinhsp89@gmail.com>
2018-10-25 08:19:21 +0000

    save generated image

--- data_augmentation.py
+++ data_augmentation.py
@@ -2,6 +2,18 @@ from common import *
 """ From URL: https://keras.io/preprocessing/image/
 """
 
+datagen = ImageDataGenerator(
+    rotation_range=90,      # can set from -90 ~ 90
+    fill_mode='nearest',    # can be one of these value: constant,nearest,reflect,wrap
+    width_shift_range=0.5,  # can be int, 1-D array, float
+    height_shift_range=0.2, # can be int, 1-D array, float
+    channel_shift_range=50, # random change color value in range(50)
+    shear_range=5,          # dont set it too big, It makes generated data isn't relevant with original data.
+    zoom_range=[1, 1.5],    # int: zoom in range[lower, upper], float: zoom in range[1-zoom_range, 1+zoom_range]    
+    horizontal_flip=True,
+    vertical_flip=True
+)
+
 def show_imgs(imgs, row, col):
     """ Show PILimage as row*col
         # Arguments
@@ -18,36 +30,31 @@ def show_imgs(imgs, row, col):
         plt.tick_params(labelbottom=False)  # remove x axis
         plt.tick_params(labelleft=False)    # remove y axis
         plt.imshow(img)
-    plt.show()
+    # plt.show()
 
+if __name__ == '__main__':
+    # files = os.listdir(os.path.join(root_path, 'data'))
+    img_path = os.path.join(root_path, 'data/lena.jpeg')
+    saved_folder = os.path.join(root_path, 'data/augmented_data')
+    os.makedirs(saved_folder, exist_ok=True)
 
-img_path = 'lena.jpeg'
-# input image, Pil format
-img = image.load_img(img_path)
-# pil format to array format
-x = image.img_to_array(img)
-#(h, w, 3) -> (1, h, w, 3)
-x = x.reshape((1,) + x.shape)
+    # input image, Pil format
+    img = image.load_img(img_path, color_mode='rgb', target_size=None, interpolation='nearest')
+    # pil format to array format
+    x = image.img_to_array(img)
+    #(h, w, 3) -> (1, h, w, 3)
+    x = x.reshape((1,) + x.shape)
 
-datagen = ImageDataGenerator(
-    rotation_range=90,      # can set from -90 ~ 90
-    fill_mode='nearest',    # can be one of these value: constant,nearest,reflect,wrap
-    width_shift_range=0.5,  # can be int, 1-D array, float
-    height_shift_range=0.2, # can be int, 1-D array, float
-    channel_shift_range=50, # random change color value in range(50)
-    shear_range=5,          # dont set it too big, It makes generated data isn't relevant with original data.
-    zoom_range=[1, 1.5],    # int: zoom in range[lower, upper], float: zoom in range[1-zoom_range, 1+zoom_range]    
-    horizontal_flip=True,
-    vertical_flip=True
-)
+    max_img_num = 16
+    imgs = []
 
-max_img_num = 16
-imgs = []
+    for i, d in enumerate(datagen.flow(x, batch_size=1)):
+        # array format to pil format and then save to saved_folder
+        img = image.array_to_img(d[0], scale=True)
+        img_name = os.path.join(saved_folder, str(i) + '.png')
+        # image.save_img(img_name, x)
+        imgs.append(img)
+        if len(imgs) % max_img_num == 0:
+            break
 
-for d in datagen.flow(x, batch_size=1):
-    # array format to pil format 
-    imgs.append(image.array_to_img(d[0], scale=True))
-    if len(imgs) % max_img_num == 0:
-        break
-
-show_imgs(imgs, row=4, col=4)
+    show_imgs(imgs, row=4, col=4)
