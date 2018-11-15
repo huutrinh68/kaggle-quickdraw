557dfe5ea4b22087f05f9c4d0097bd2690d270e0
huutrinh68 <trinhsp89@gmail.com>
2018-11-15 23:41:21 +0900

    MobileNet with imageNet weight

--- data_loader/simple_data_loader.py
+++ data_loader/simple_data_loader.py
@@ -7,17 +7,20 @@ import cv2
 import ast
 import numpy as np
 from tensorflow import keras
+from tqdm import tqdm
 
-root_path = '/home/trinhnh1/Documents/train_data/kaggle/quick_draw/'
+root_path = '/media/trinhnh1/3A08638408633DCF/train_data/kaggle/quick_draw'
 SHUFFLE_DATA = os.path.join(root_path,'input/shuffle_data/')
 NCSVS = 100
-BASE_SIZE = 256
-size = 64
+BASE_SIZE = 1024
+size = 224
 def df_to_image_array_xd(df, size, lw=6, time_color=True):
     df['drawing'] = df['drawing'].apply(ast.literal_eval)
-    x = np.zeros((len(df), size, size, 1))
+    x = np.zeros((len(df), size, size, 3))
     for i, raw_strokes in enumerate(df.drawing.values):
         x[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
+        x[i, :, :, 1] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
+        x[i, :, :, 2] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
     x = preprocess_input(x).astype(np.float32)
     return x
 
@@ -36,13 +39,15 @@ def draw_cv2(raw_strokes, size=256, lw=6, time_color=True):
 
 def image_generator_xd(size, batchsize, ks, lw=6, time_color=True):
     while True:
-        for k in np.random.permutation(ks):
+        for k in tqdm(np.random.permutation(ks)):
             filename = os.path.join(SHUFFLE_DATA, 'train_k{}.csv.gz'.format(k))
             for df in pd.read_csv(filename, chunksize=batchsize):
                 df['drawing'] = df['drawing'].apply(ast.literal_eval)
-                X_train = np.zeros((len(df), size, size, 1)) # for MobileNet
+                X_train = np.zeros((len(df), size, size, 3))
                 for i, raw_strokes in enumerate(df.drawing.values):
                     X_train[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
+                    X_train[i, :, :, 1] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
+                    X_train[i, :, :, 2] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
                 X_train = preprocess_input(X_train).astype(np.float32)
                 y_train = keras.utils.to_categorical(df.y, num_classes=340)
                 yield X_train, y_train
@@ -53,8 +58,8 @@ class MobileNetDataLoader(BaseDataLoader):
         super(MobileNetDataLoader, self).__init__(config)
         test_df = pd.read_csv(os.path.join(SHUFFLE_DATA, 'train_k{}.csv.gz'.format(NCSVS - 1)), nrows=34000)
         self.X_test = df_to_image_array_xd(test_df, size)
-        self.y_test = keras.utils.to_categorical(test_df.y, num_classes=340)
-        self.train_datagen = image_generator_xd(size=size, batchsize=256, ks=range(NCSVS - 1))
+        self.y_test = keras.utils.to_categorical(test_df.y, num_classes=config.data_atr.num_classes)
+        self.train_datagen = image_generator_xd(size=size, batchsize=config.trainer.batch_size, ks=range(NCSVS - 1))
 
     def get_train_data(self):
         return self.train_datagen
--- models/simple_model.py
+++ models/simple_model.py
@@ -1,7 +1,7 @@
 from base.base_model import BaseModel
-from keras.models import Sequential
-from keras.layers import Input, Dense, GlobalAveragePooling2D
-from keras.applications import MobileNet
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Input, BatchNormalization, GlobalAveragePooling2D
+from keras.applications import MobileNet, NASNetLarge
 from keras.optimizers import Adam
 from keras.metrics import categorical_accuracy, top_k_categorical_accuracy, categorical_crossentropy
 
@@ -11,17 +11,29 @@ def top_3_accuracy(y_true, y_pred):
 class MobileNetModel(BaseModel):
     def __init__(self, config):
         super(MobileNetModel, self).__init__(config)
+        self.num_classes = config.data_attr.num_classes
         self.build_model()
     
     def build_model(self):
         img_size = self.config.trainer.img_size
         num_classes = self.config.data_attr.num_classes
+        input_tensor = Input(shape=(img_size, img_size, 3))
         self.model = MobileNet(
-            input_shape=(img_size, img_size, 1), 
+            include_top=False,
+            input_shape=(img_size, img_size, 3), 
             alpha=self.config.model.alpha, 
-            weights=None,
-            classes=num_classes
+            weights='imagenet'
         )
+        bn = BatchNormalization()(input_tensor)
+        x = self.model(bn)
+        x = Conv2D(32, kernel_size=(1,1), activation='relu')(x)
+        x = Flatten()(x)
+        x = Dropout(0.5)(x)
+        x = Dense(1024, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        output = Dense(self.num_classes, activation='sigmoid')(x)
+        self.model = Model(input_tensor, output)
+        
         self.model.compile(
             optimizer=Adam(lr=self.config.model.learning_rate), 
             loss=self.config.trainer.loss,
