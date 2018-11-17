c5dda4bfcfa902b2e12bed6107eb382c17b6b784
huutrinh68 <trinhsp89@gmail.com>
2018-11-17 20:53:31 +0900

    reconstruct

--- main.py
+++ main.py
@@ -33,6 +33,28 @@ def main():
     print("Start training the model.")
     trainer.train()
 
+    # validation
+    valid_predictions = model.predict(x_valid, batch_size=128, verbose=1)
+    map3 = mapk(valid_df[['y']].values, preds2catids(valid_predictions).values)
+
+    # create submission
+    test = pd.read_csv(os.path.join(INPUT_DIR, 'test_simplified.csv'))
+    test.head()
+    x_test = df_to_image_array_xd(test, size)
+    test_predictions = model.predict(x_test, batch_size=128, verbose=1)
+    top3 = preds2catids(test_predictions)
+    cats = Simplified().list_all_categories()
+    id2cat = {k: cat.replace(' ', '_') for k, cat in enumerate(cats)}
+    top3cats = top3.replace(id2cat)
+    test['word'] = top3cats['a'] + ' ' + top3cats['b'] + ' ' + top3cats['c']
+    submission = test[['key_id', 'word']]
+    submission.to_csv('gs_mn_submission_{}.csv'.format(int(map3 * 10**4)), index=False)
+
+    # show process time
+    end = dt.datetime.now()
+    print('Latest run {}.\nTotal time {}s'.format(end, (end - start).seconds))
+
+
 
 if __name__ == "__main__":
     main()
